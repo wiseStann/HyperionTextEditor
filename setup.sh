27dc59860083d7ba54f7256a27a35faae1b952be
Stacey Kerr <servicename18@gmail.com>
2021-08-28 00:29:40 +0300

    update setup configuration: add file existence check

--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-sudo mv hyper.gschema.xml /usr/share/glib-2.0/schemas
-sudo glib-compile-schemas /usr/share/glib-2.0/schemas/
-sudo glib-compile-resources styles/hyper.gresource.xml --target=src/resources.c --generate-source
+SCHEMA_FILE="hyper.gschema.xml"
+SCHEMAS_DIR_PATH="/usr/share/glib-2.0/schemas"
+
+GRESOURCE_FILE="hyper.gresource.xml"
+
+if [ ! -f $SCHEMAS_DIR_PATH/$FILE ]; then
+    sudo cp $SCHEMA_FILE $SCHEMAS_DIR_PATH/$SCHEMA_FILE
+fi
+
+sudo glib-compile-schemas $SCHEMAS_DIR_PATH
+sudo glib-compile-resources $GRESOURCE_FILE --target=src/resources.c --generate-source
