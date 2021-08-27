2c3a5daed94006a29412bbf1a6ee89a9f3437309
Stacey Kerr <servicename18@gmail.com>
2021-08-27 21:41:33 +0300

    pushing up to the github

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+sudo mv hyper.gschema.xml /usr/share/glib-2.0/schemas
 sudo glib-compile-schemas /usr/share/glib-2.0/schemas/
-sudo glib-compile-resources styles/ms.gresource.xml --target=src/resources.c --generate-source
+sudo glib-compile-resources styles/hyper.gresource.xml --target=src/resources.c --generate-source
